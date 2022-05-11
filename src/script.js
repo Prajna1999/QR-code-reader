63b6843f3d33a4f4fee7af750968e15ad6013f73
Prajna1999 <gituprajna20@gmail.com>
2022-05-11 07:41:09 +0530

    Func: copy, API call and bug fixes

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 const wrapper=document.querySelector(".wrapper");
 const inputFile=document.querySelector("form input");
 const form=document.querySelector("form");
-
+const infoText=document.querySelector(".content p");
+const textArea=document.querySelector(".details textarea");
+console.log(textArea);
 
 
 // generic fetch request function.
-function fetchRequest(formData){
+function fetchRequest(formData,file){
+    infoText.innerText="Scanning QR Code..."
     const req=new Request("http://api.qrserver.com/v1/read-qr-code/",{
         method:"POST",
         body:formData
@@ -18,10 +21,20 @@ function fetchRequest(formData){
                 return result.json();
             }
         }).then(data=>{
+            const dataField=data[0].symbol[0].data;
+            infoText.innerText=dataField?"Upload QR Code to Scan":"Cuoldn't scan QR Code";
+            if(!dataField) return;
+            textArea.innerText=dataField;
+            document.querySelector("img").src=URL.createObjectURL(file)
+            
+            wrapper.classList.add("active");
             console.log(data);
-        }).catch(e=>console.log("error:", e.message))
+        }).catch((e)=>{
+            // Code 404
+            console.log(e.message);
+            infoText.textArea="Couldn't scan the QR Code"
+        })
 }
-
 
 
 
@@ -31,14 +44,21 @@ function fetchRequest(formData){
 // chage event on the fileInput.
 inputFile.addEventListener("change", (e)=>{
     const file=e.target.files[0];
+    if(!file) return;
     const formData=new FormData();
     formData.append("file", file);
-    fetchRequest(formData);
+    fetchRequest(formData,file);
     
 })
 
-// Event listener on form submit event.
+// Event listener on form click event.
 form.addEventListener("click",()=>{
     
     inputFile.click();
+})
+document.getElementById("close").addEventListener("click", ()=>{
+    wrapper.classList.remove("active");
+});
+document.getElementById("copy").addEventListener("click", (e)=>{
+    navigator.clipboard.writeText(textArea.textContent);
 })
